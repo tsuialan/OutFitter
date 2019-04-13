34d199366b6338c7163439d13c5b079449f802f9
Alan Tsui <alantsui18@gmail.com>
2019-04-13 00:14:12 -0700

    added python script

---
+++ script-console.py
@@ -0,0 +1,4 @@
+import os
+os.system("curl -v -s -H 'Content-Type: application/json' \
+    https://vision.googleapis.com/v1/images:annotate?key=AIzaSyCgYLR6tIgUSpuNyTadxfuLadPDXSLP8OI \
+    --data-binary @request.json > response.json")
